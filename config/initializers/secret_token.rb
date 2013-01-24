82e5b649e928d8cfa0d5f4e70d25f2639a210813
iven <ivenvd@gmail.com>
2013-01-24 11:03:47 +0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CnBetter::Application.config.secret_token = '60476b79ad083e749aeffdec08e9b3e051bd8c812adebab8010feb0e9333aa3e47fb272a101e9572feead6ad319997ac668cd9f6728fe842d3c6198133720211'
