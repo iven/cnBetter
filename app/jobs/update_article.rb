bb5d3336d4011e93423c0ba65efb9c1a8ef978bc
iven <ivenvd@gmail.com>
2013-02-04 20:51:06 +0800

    Use nokogiri to parse and format articles.

--- app/jobs/update_article.rb
+++ app/jobs/update_article.rb
@@ -5,7 +5,16 @@ require 'nokogiri'
 
 module UpdateArticle
   @queue = :update_article
-  @re_article = %r{<b>(?<title>.*)</b><br/><b>新闻发布日期：</b>(?<published_on>.*)<br/><b>新闻主题：</b> (?<topic>.*) <br/><br/> (<b>感谢(?<author>.*?)的投递</b><br />)?(?<content>.*)<a href="hcomment}m
+
+  def self.format_paragraph!(doc)
+    doc.search("//br/preceding-sibling::text()|//br/following-sibling::text()").each do |node|
+      content = node.to_html.strip.gsub('　', '')
+      unless content.empty?
+        node.replace(Nokogiri.make("<p>#{content}</p>"))
+      end
+    end
+    doc.css('br').remove
+  end
 
   def self.format_image!(doc)
     doc.css('img').each do |img|
@@ -22,21 +31,42 @@ module UpdateArticle
 
     uri = "http://m.cnbeta.com/marticle.php?sid=#{id}"
 
-    md = open(uri).read.force_encoding('utf-8').match(@re_article)
+    doc = Nokogiri::XML(open(uri).read.force_encoding('utf-8'))
+    self.format_image! doc
+    self.format_paragraph! doc
+
+    card = doc.at_xpath('/wml/card/p')
+    card.name = 'div'
+    card['class'] = 'content'
+
+    card.children[-3..-1].remove # 查看评论、返回首页
+    card.child.remove # 返回首页
+    title = card.child.remove.text.strip
+    card.child.remove # 新闻发布日期
+    published_on = card.child.remove.text.strip
+    card.child.remove # 新闻主题
+    topic = card.child.remove.text.strip
+    card.child.remove # 空
+
+    author = nil
+    md = card.child.text.match(/感谢(?<author>.*)的投递/)
     if md
-      hash = Hash[ md.names.zip(md.captures) ]
+      author = md['author'].strip
+      card.child.remove
+    end
 
-      doc = Nokogiri::HTML.fragment(hash['content'])
-      self.format_image! doc
+    article = Article.create!(
+      id_: id,
+      title: title,
+      author: author,
+      content: card.to_html,
+      published_on: published_on,
+    )
 
-      hash['content'] = doc.to_html.strip
-      hash['id_'] = Integer(id)
-
-      Article.create!(hash)
-
-      unless Topic.where(name: md['topic']).exists?
-        Resque.enqueue(UpdateTopic, hash['id_'])
-      end
+    topic = Topic.find_or_create_by(name: topic)
+    topic.articles << article
+    unless topic.image_url
+      Resque.enqueue(UpdateTopic, id)
     end
   end
 end
--- app/jobs/update_topic.rb
+++ app/jobs/update_topic.rb
@@ -10,14 +10,16 @@ module UpdateTopic
   def self.perform(article_id)
     uri = "http://cnbeta.com/articles/#{article_id}.htm"
 
-    md = open(uri).read.force_encoding('gb18030').match(@re_article)
+    md = open(uri).read.force_encoding('gb18030').encode!('utf-8').match(@re_article)
     if md
-      hash = Hash[ md.names.zip(md.captures) ]
-      unless Topic.where(name: md['name']).exists?
-        Topic.create!(hash)
+      Topic.find_by(name: md['name']) do |topic|
+        image_url = md['image_url']
+        unless topic.image_url
+          topic.update_attributes!(image_url: image_url)
 
-        unless Image.where(uri: md['image_url']).exists?
-          Resque.enqueue(UpdateImage, md['image_url'])
+          unless Image.where(uri: image_url).exists?
+            Resque.enqueue(UpdateImage, image_url)
+          end
         end
       end
     end
